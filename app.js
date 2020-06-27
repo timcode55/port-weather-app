c86176e693b39448504e1b5824a15e1aa6abfdfd
Tim Petrvalsky <tpet55@yahoo.com>
2020-06-26 18:26:06 -0700

    CORS working

--- app.js
+++ app.js
@@ -1,160 +1,51 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+// const request = require('request-promise');
+
 // FETCH BACKGROUND IMAGE FOR STATIC HOME PAGE
 
-const urlBackground =
-	'https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
+const PORT = process.env.PORT || 9000;
 
-document.body.style.backgroundImage = `url('${urlBackground}')`;
-document.body.style.backgroundSize = 'cover';
-document.body.style.height = '100vh';
-const messageOne = document.querySelector('#message-one');
-let forecast = document.querySelector('#target');
-const messageTwo = document.querySelector('#message-two');
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
+let app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname + '/static')));
+// CORS SOLUTION
 
-let display = [];
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+app.get('/weathercors', (req, res) => {
+	url =
+		'https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
+	request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
 
-weatherForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const location = search.value;
-	getLatLong(location);
-	search.value = '';
-	const randomPage = Math.floor(Math.random() * 10);
-	let editTitle = document.querySelector('.title');
-	editTitle.classList.add('background-title');
-	editTitle.innerHTML = `${location} local weather`;
+		res.json(JSON.parse(body));
+	});
+});
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+app.get('/weatherdark', (req, res) => {
+	url = `https://api.darksky.net/forecast/757c5a5c185c90b230b94d7c401fe771/36,-121`;
+	request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
 
-	// FETCH UNSPLASH API DATA FOR BACKGROUND IMAGE FROM QUERIED CITY
-
-	fetch(
-		'https://cors-anywhere.herokuapp.com/https://api.unsplash.com/search/photos?page=' +
-			randomPage +
-			'&query=' +
-			location +
-			'&client_id=LyTILpYq9RlxI2Zefq96p9KRqORAkQyQ6cYqjngIUVg'
-	).then((response) => {
-		response.json().then((data) => {
-			const random = Math.floor(Math.random() * 10);
-			let result = data.results[random].urls.full;
-			// let resultMiami =
-			// 	'https://images.unsplash.com/photo-1543968332-f99478b1ebdc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
-			// let resultLA = 'https://images.unsplash.com/photo-1534190760961-74e8c1c5c3da?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0'
-			document.body.style.backgroundImage = `url('${result}')`;
-			document.body.style.backgroundSize = 'cover';
-			document.body.style.height = '100vh';
-			getAccu(location);
-		});
+		res.json(JSON.parse(body));
 	});
 });
 
-// GET LATITUDE AND LONGITUDE FOR ENTERED LOCATION
+// app.get('/weather', function(req, res) {
+// 	res.sendFile(path.join(__dirname + '/static/index.html'));
+// });
 
-let getLatLong = (location) => {
-	fetch(
-		'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-			encodeURIComponent(location) +
-			'.json?access_token=pk.eyJ1IjoicGV0bWFwNTUiLCJhIjoiY2s3NmZiYzl2MHFpZTNtbzNycHh0aDlnMCJ9.oERa5PVzkVL4oEZXMOWnxA&limit=1'
-	).then((response) => {
-		response.json().then((data) => {
-			let latitude = data.features[0].bbox[1];
-			let longitude = data.features[0].bbox[0];
-			fetchData(latitude, longitude);
-		});
-	});
-};
-
-let latitude = 36.5964139832728;
-let longitude = -121.943692018703;
-
-// GET FORECAST WITH THE LATITUDE AND LONGITUDE
-
-let fetchData = (latitude, longitude) => {
-	fetch(
-		'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/757c5a5c185c90b230b94d7c401fe771/' +
-			latitude +
-			',' +
-			longitude
-	).then((response) => {
-		response.json().then((data) => {
-			display = [];
-			display.push({ 'Forecast Today': data.currently.summary });
-			display.push({ 'Current Temp': data.currently.temperature });
-			display.push({ 'Wind Speed': data.currently.windSpeed });
-			display.push({ 'Wind Gust': data.currently.windGust });
-			display.push({ Tomorrow: data.daily.data[1].summary });
-			display.push({ 'Cur Humidity': data.currently.humidity * 100 });
-			display.push({ 'Dew Point': data.currently.dewPoint });
-			display.push({ 'UV Index': data.currently.uvIndex });
-			display.push({ 'Ozone Level': data.currently.ozone });
-			display.push({ Visibility: data.currently.visibility });
-			display.push({ Pressure: data.currently.pressure });
-			display.push({ 'Cloud Cover': parseInt(data.currently.cloudCover * 100) });
-			display.push({ Sunset: new Date(data.daily.data[0].sunsetTime * 1000).toLocaleString() });
-			displayData();
-		});
-	});
-};
-
-// GET ACCUWEATHER DATA FOR THE POLLEN LEVELS
-
-let getAccu = (location) => {
-	fetch(
-		'https://cors-anywhere.herokuapp.com/https://dataservice.accuweather.com/locations/v1/cities/search?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&q=' +
-			location +
-			'&details=true'
-	).then((response) => {
-		response.json().then((data) => {
-			let locationKey = data[0].Key;
-			pollenForecast(locationKey);
-		});
-	});
-	let pollenForecast = (locationKey) => {
-		fetch(
-			'https://cors-anywhere.herokuapp.com/https://dataservice.accuweather.com/forecasts/v1/daily/1day/' +
-				locationKey +
-				'?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&details=true'
-		).then((response) => {
-			response.json().then((data) => {
-				display.push({
-					'Air Quality': `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
-						.AirAndPollen[0].Category}`
-				});
-				display.push({
-					Grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
-						.Category}`
-				});
-				display.push({
-					Mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
-						.Category}`
-				});
-				display.push({
-					Ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
-						.Category}`
-				});
-				display.push({
-					Tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
-						.Category}`
-				});
-				displayData();
-			});
-		});
-	};
-};
-
-// DISPLAY DATA ON THE PAGE
-
-function displayData() {
-	let div = document.createElement('div');
-	for (let item of display) {
-		for (let key in item) {
-			div.innerHTML += `<article class="notification is-primary today">
-  <p class="forecastTitle">${key}</p>
-  <p class="forecast">${item[key]}</p>
-  </article>`;
-		}
-	}
-
-	div.classList.add('main-div');
-	target.innerHTML = '';
-	target.appendChild(div);
-}
+app.listen(PORT, () => {
+	console.log('Server is up on port ' + PORT);
+});
---
+++ static/weather.js
@@ -0,0 +1,158 @@
+const urlBackground =
+	'https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
+
+document.body.style.backgroundImage = `url('${urlBackground}')`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.height = '100vh';
+const messageOne = document.querySelector('#message-one');
+let forecast = document.querySelector('#target');
+const messageTwo = document.querySelector('#message-two');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+let display = [];
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const location = search.value;
+	getLatLong(location);
+	search.value = '';
+	const randomPage = Math.floor(Math.random() * 10);
+	let editTitle = document.querySelector('.title');
+	editTitle.classList.add('background-title');
+	editTitle.innerHTML = `${location} local weather`;
+
+	// FETCH UNSPLASH API DATA FOR BACKGROUND IMAGE FROM QUERIED CITY
+
+	fetch(
+		'https://api.unsplash.com/search/photos?page=' +
+			randomPage +
+			'&query=' +
+			location +
+			'&client_id=LyTILpYq9RlxI2Zefq96p9KRqORAkQyQ6cYqjngIUVg'
+	).then((response) => {
+		response.json().then((data) => {
+			const random = Math.floor(Math.random() * 10);
+			let result = data.results[random].urls.full;
+			// let resultMiami =
+			// 	'https://images.unsplash.com/photo-1543968332-f99478b1ebdc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
+			// let resultLA = 'https://images.unsplash.com/photo-1534190760961-74e8c1c5c3da?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0'
+			document.body.style.backgroundImage = `url('${result}')`;
+			document.body.style.backgroundSize = 'cover';
+			document.body.style.height = '100vh';
+			getAccu(location);
+		});
+	});
+});
+
+// GET LATITUDE AND LONGITUDE FOR ENTERED LOCATION
+
+let getLatLong = (location) => {
+	fetch(
+		'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+			encodeURIComponent(location) +
+			'.json?access_token=pk.eyJ1IjoicGV0bWFwNTUiLCJhIjoiY2s3NmZiYzl2MHFpZTNtbzNycHh0aDlnMCJ9.oERa5PVzkVL4oEZXMOWnxA&limit=1'
+	).then((response) => {
+		response.json().then((data) => {
+			let latitude = data.features[0].bbox[1];
+			let longitude = data.features[0].bbox[0];
+			fetchData(latitude, longitude);
+		});
+	});
+};
+
+let latitude = 36.5964139832728;
+let longitude = -121.943692018703;
+
+// GET FORECAST WITH THE LATITUDE AND LONGITUDE
+
+async function fetchData(latitude, longitude) {
+	const testingData = await axios
+		.get('/weatherdark/')
+		.then((response) => {
+			console.log(response);
+			console.log(response.data.currently.apparentTemperature);
+			display = [];
+			display.push({ 'Forecast Today': response.data.currently.summary });
+			display.push({ 'Current Temp': response.data.currently.temperature });
+			display.push({ 'Wind Speed': response.data.currently.windSpeed });
+			display.push({ 'Wind Gust': response.data.currently.windGust });
+			display.push({ Tomorrow: response.data.daily.data[1].summary });
+			display.push({ 'Cur Humidity': response.data.currently.humidity * 100 });
+			display.push({ 'Dew Point': response.data.currently.dewPoint });
+			display.push({ 'UV Index': response.data.currently.uvIndex });
+			display.push({ 'Ozone Level': response.data.currently.ozone });
+			display.push({ Visibility: response.data.currently.visibility });
+			display.push({ Pressure: response.data.currently.pressure });
+			display.push({ 'Cloud Cover': parseInt(response.data.currently.cloudCover * 100) });
+			display.push({ Sunset: new Date(response.data.daily.data[0].sunsetTime * 1000).toLocaleString() });
+			displayData();
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+
+// GET ACCUWEATHER DATA FOR THE POLLEN LEVELS
+
+let getAccu = (location) => {
+	fetch(
+		'https://dataservice.accuweather.com/locations/v1/cities/search?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&q=' +
+			location +
+			'&details=true'
+	).then((response) => {
+		response.json().then((data) => {
+			let locationKey = data[0].Key;
+			pollenForecast(locationKey);
+		});
+	});
+	let pollenForecast = (locationKey) => {
+		fetch(
+			'https://dataservice.accuweather.com/forecasts/v1/daily/1day/' +
+				locationKey +
+				'?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&details=true'
+		).then((response) => {
+			response.json().then((data) => {
+				display.push({
+					'Air Quality': `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
+						.AirAndPollen[0].Category}`
+				});
+				display.push({
+					Grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
+						.Category}`
+				});
+				display.push({
+					Mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
+						.Category}`
+				});
+				display.push({
+					Ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
+						.Category}`
+				});
+				display.push({
+					Tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
+						.Category}`
+				});
+				displayData();
+			});
+		});
+	};
+};
+
+// DISPLAY DATA ON THE PAGE
+
+function displayData() {
+	let div = document.createElement('div');
+	for (let item of display) {
+		for (let key in item) {
+			div.innerHTML += `<article class="notification is-primary today">
+  <p class="forecastTitle">${key}</p>
+  <p class="forecast">${item[key]}</p>
+  </article>`;
+		}
+	}
+
+	div.classList.add('main-div');
+	target.innerHTML = '';
+	target.appendChild(div);
+}
