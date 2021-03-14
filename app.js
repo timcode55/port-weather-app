ced65450ad8ba2e06892532c13e110337e6f0c46
Tim Petrvalsky <tpet55@yahoo.com>
2021-03-14 16:14:23 -0700

    Finish grid display

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 require('dotenv').config();
+const axios = require('axios');
 
 // FETCH BACKGROUND IMAGE FOR STATIC HOME PAGE
 
@@ -31,9 +32,27 @@ app.get('/weatherdark/:latlon', async (req, res) => {
 	});
 });
 
-// app.get('/weather', function(req, res) {
-// 	res.sendFile(path.join(__dirname + '/static/index.html'));
-// });
+app.get('/background', function(req, res) {
+	const randomPage = Math.floor(Math.random() * 10);
+	let background = axios(
+		'https://api.unsplash.com/search/photos?page=' +
+			randomPage +
+			'&query=' +
+			'miami' +
+			'&client_id=LyTILpYq9RlxI2Zefq96p9KRqORAkQyQ6cYqjngIUVg'
+	).then((response) => {
+		(response) => response.data;
+		const random = Math.floor(Math.random() * 10);
+		let result = response.data.results[random].urls.full;
+		res.send(result);
+		// let resultMiami =
+		// 	'https://images.unsplash.com/photo-1543968332-f99478b1ebdc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
+		// let resultLA = 'https://images.unsplash.com/photo-1534190760961-74e8c1c5c3da?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0'
+		// document.body.style.backgroundImage = `url('${result}')`;
+		// document.body.style.backgroundSize = 'center/cover';
+		// document.body.style.height = '100vh';
+	});
+});
 
 app.listen(PORT, () => {
 	console.log('Server is up on port ' + PORT);
--- static/weather.js
+++ static/weather.js
@@ -1,6 +1,15 @@
 const urlBackground =
 	'https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
 
+// const getBackground = () => {
+// 	axios.get('/background').then((res) => {
+// 		console.log(res.data);
+// 		document.body.style.backgroundImage = `url('${res.data}')`;
+// 	});
+// };
+
+// getBackground();
+
 document.body.style.backgroundImage = `url('${urlBackground}')`;
 document.body.style.backgroundSize = 'cover';
 document.body.style.height = '100vh';
@@ -40,7 +49,7 @@ weatherForm.addEventListener('submit', (e) => {
 			// 	'https://images.unsplash.com/photo-1543968332-f99478b1ebdc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
 			// let resultLA = 'https://images.unsplash.com/photo-1534190760961-74e8c1c5c3da?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0'
 			document.body.style.backgroundImage = `url('${result}')`;
-			document.body.style.backgroundSize = 'cover';
+			document.body.style.backgroundSize = 'center/cover';
 			document.body.style.height = '100vh';
 		});
 	});
@@ -71,23 +80,26 @@ async function fetchData(lat, lon) {
 	const testingData = await axios
 		.get(`/weatherdark/${lat},${lon}`)
 		.then((response) => {
-			display.push({ 'Week Summary': response.data.daily.summary });
-			display.push({ 'Forecast Today': response.data.currently.summary });
-			display.push({ Tomorrow: response.data.daily.data[1].summary });
-			display.push({ 'Current Temp': response.data.currently.temperature });
-			display.push({ 'Min Temp': response.data.daily.data[0].temperatureMin });
-			display.push({ 'Max Temp': response.data.daily.data[0].temperatureMax });
-			display.push({ 'Wind Speed': response.data.currently.windSpeed });
-			display.push({ 'Wind Gust': response.data.currently.windGust });
+			display.push({ weekSummary: response.data.daily.summary });
+			display.push({ forecastToday: response.data.currently.summary });
+			display.push({ forecastTomorrow: response.data.daily.data[1].summary });
+			display.push({ currentTemp: response.data.currently.temperature });
+			display.push({ minTemp: Math.round(response.data.daily.data[0].temperatureMin) });
+			display.push({ maxTemp: Math.round(response.data.daily.data[0].temperatureMax) });
+			display.push({ windSpeed: response.data.currently.windSpeed });
+			display.push({ windGust: response.data.currently.windGust });
 			display.push({
-				'Wind Gust Time': new Date(response.data.daily.data[0].windGustTime * 1000).toLocaleString()
+				windGustTime: new Date(response.data.daily.data[0].windGustTime * 1000).toLocaleString()
 			});
-			display.push({ 'Cur Humidity': (response.data.currently.humidity * 100).toFixed(2) });
-			display.push({ 'Dew Point': response.data.currently.dewPoint });
-			display.push({ Sunrise: new Date(response.data.daily.data[0].sunriseTime * 1000).toLocaleString() });
-			displayData(display);
-			display.push({ Sunset: new Date(response.data.daily.data[0].sunsetTime * 1000).toLocaleString() });
-			displayData(display);
+			display.push({ humidity: (response.data.currently.humidity * 100).toFixed(2) });
+			display.push({ dewPoint: response.data.currently.dewPoint });
+			display.push({ visibility: response.data.currently.visibility });
+			display.push({ ozone: response.data.currently.ozone });
+			display.push({ sunrise: new Date(response.data.daily.data[0].sunriseTime * 1000).toLocaleString() });
+			// displayData(display);
+			display.push({ sunset: new Date(response.data.daily.data[0].sunsetTime * 1000).toLocaleString() });
+			// displayData(display);
+			temp(display);
 		})
 		.catch((error) => {
 			console.log(error);
@@ -115,23 +127,23 @@ let getAccu = async (location) => {
 		).then((response) => {
 			response.json().then((data) => {
 				display.push({
-					'Air Quality': `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
+					airQuality: `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
 						.AirAndPollen[0].Category}`
 				});
 				display.push({
-					Grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
+					grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
 						.Category}`
 				});
 				display.push({
-					Mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
+					mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
 						.Category}`
 				});
 				display.push({
-					Ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
+					ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
 						.Category}`
 				});
 				display.push({
-					Tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
+					tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
 						.Category}`
 				});
 				// displayData();
@@ -141,6 +153,56 @@ let getAccu = async (location) => {
 };
 
 // DISPLAY DATA ON THE PAGE
+
+function temp(display) {
+	let toggle = document.querySelectorAll('.hidden');
+	toggle.forEach((item) => {
+		item.classList.toggle('hidden');
+	});
+	console.log(display, 'display');
+	let temp = document.getElementById('temp');
+	let tempMin = document.getElementById('temp-min');
+	let tempMax = document.getElementById('temp-max');
+	let weekSummary = document.getElementById('week-summary');
+	let airQuality = document.getElementById('air-quality');
+	let grass = document.getElementById('grass');
+	let mold = document.getElementById('mold');
+	let ragweed = document.getElementById('ragweed');
+	let tree = document.getElementById('tree');
+	let sunrise = document.getElementById('sunrise');
+	let sunset = document.getElementById('sunset');
+	let humidity = document.getElementById('humidity');
+	let windGust = document.getElementById('wind-gust');
+	let windGustTime = document.getElementById('wind-gust-time');
+	let windSpeed = document.getElementById('wind-speed');
+	let dewPoint = document.getElementById('dew-point');
+	let forecastToday = document.getElementById('forecast-today');
+	let forecastTomorrow = document.getElementById('forecast-tomorrow');
+	let currentTemp = document.getElementById('current-temp');
+	let visibility = document.getElementById('visibility');
+	let ozone = document.getElementById('ozone');
+	// temp.textContent = display[10].maxTemp;
+	tempMin.textContent = display[9].minTemp;
+	tempMax.textContent = display[10].maxTemp;
+	weekSummary.textContent = display[5].weekSummary;
+	airQuality.textContent = display[0].airQuality;
+	grass.textContent = display[1].grass;
+	mold.textContent = display[2].mold;
+	ragweed.textContent = display[3].ragweed;
+	tree.textContent = display[4].tree;
+	sunrise.textContent = display[18].sunrise;
+	sunset.textContent = display[19].sunset;
+	humidity.textContent = display[14].humidity;
+	windGust.textContent = display[12].windGust;
+	windGustTime.textContent = display[13].windGustTime;
+	windSpeed.textContent = display[11].windSpeed;
+	dewPoint.textContent = display[15].dewPoint;
+	forecastToday.textContent = display[6].forecastToday;
+	forecastTomorrow.textContent = display[7].forecastTomorrow;
+	currentTemp.textContent = display[8].currentTemp;
+	visibility.textContent = display[16].visibility;
+	ozone.textContent = display[17].ozone;
+}
 
 function displayData(display) {
 	let div = document.createElement('div');
