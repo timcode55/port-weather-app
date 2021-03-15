fea5b7148894b45529a43e8ee82ed59dd5103b78
Tim Petrvalsky <tpet55@yahoo.com>
2021-03-15 11:54:46 -0700

    Get all routes working

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
+
 app.get('/weatherdark/:latlon', async (req, res) => {
 	const latlon = req.params.latlon.split(',');
 	const lat = latlon[0];
@@ -31,9 +32,56 @@ app.get('/weatherdark/:latlon', async (req, res) => {
 	});
 });
 
-// app.get('/weather', function(req, res) {
-// 	res.sendFile(path.join(__dirname + '/static/index.html'));
-// });
+app.get('/accu/:location', async (req, res) => {
+	const location = req.params.location;
+	let api_key = process.env.ACCU_KEY;
+	url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${api_key}&q=${location}&details=true`;
+	await request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
+		res.json(JSON.parse(body));
+	});
+});
+
+app.get('/accuweather/:locationKey', async (req, res) => {
+	const locationKey = req.params.locationKey;
+	let api_key = process.env.ACCU_KEY;
+	url = `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${api_key}&details=true`;
+	await request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
+		res.json(JSON.parse(body));
+	});
+});
+
+app.get('/unsplash/:location', async (req, res) => {
+	const location = req.params.location;
+	let api_key = process.env.UNSPLASH_KEY;
+	const randomPage = Math.floor(Math.random() * 10);
+	url = `https://api.unsplash.com/search/photos?page=${randomPage}&query=${location}&client_id=${api_key}`;
+	await request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
+		res.json(JSON.parse(body));
+	});
+});
+
+app.get('/mapbox/:location', async (req, res) => {
+	const location = req.params.location;
+	let api_key = process.env.MAPBOX_KEY;
+	url = `https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+  encodeURIComponent(${location}) +
+  '.json?access_token=${api_key}&limit=1`;
+	await request({ url }, (error, response, body) => {
+		if (error || response.statusCode !== 200) {
+			return res.status(500).json({ type: 'error', message: err.message });
+		}
+		res.json(JSON.parse(body));
+	});
+});
 
 app.listen(PORT, () => {
 	console.log('Server is up on port ' + PORT);
--- static/weather.js
+++ static/weather.js
@@ -1,3 +1,4 @@
+// INITIAL BACKGROUND IMAGE
 const urlBackground =
 	'https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
 
@@ -9,53 +10,40 @@ const search = document.querySelector('input');
 
 let display = [];
 
-weatherForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	display = [];
-	const location = search.value;
-	getLatLong(location);
-	getAccu(location);
-	search.value = '';
-	const randomPage = Math.floor(Math.random() * 10);
-	let editTitle = document.querySelector('.title');
-	editTitle.classList.add('background-title');
-	editTitle.innerHTML = `${location} local weather`;
+// SEARCH FORM FOR CITY WEATHER FORECAST
+const weatherSearch = async () => {
+	weatherForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		display = [];
+		const location = search.value;
+		getLatLong(location);
+		getAccu(location);
+		search.value = '';
+		const randomPage = Math.floor(Math.random() * 10);
+		let editTitle = document.querySelector('.title');
+		editTitle.classList.add('background-title');
+		editTitle.innerHTML = `${location} local weather`;
 
-	// FETCH UNSPLASH API DATA FOR BACKGROUND IMAGE FROM QUERIED CITY
-
-	fetch(
-		'https://api.unsplash.com/search/photos?page=' +
-			randomPage +
-			'&query=' +
-			location +
-			'&client_id=LyTILpYq9RlxI2Zefq96p9KRqORAkQyQ6cYqjngIUVg'
-	).then((response) => {
-		response.json().then((data) => {
+		// FETCH UNSPLASH API DATA FOR BACKGROUND IMAGE FROM QUERIED CITY
+		axios.get(`/unsplash/${location}`).then((response) => {
 			const random = Math.floor(Math.random() * 10);
-			let result = data.results[random].urls.full;
-			// let resultMiami =
-			// 	'https://images.unsplash.com/photo-1543968332-f99478b1ebdc?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0';
-			// let resultLA = 'https://images.unsplash.com/photo-1534190760961-74e8c1c5c3da?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyNDk1NX0'
+			let result = response.data.results[random].urls.full;
 			document.body.style.backgroundImage = `url('${result}')`;
 			document.body.style.backgroundSize = 'cover';
 			document.body.style.height = '100vh';
 		});
 	});
-});
+};
+weatherSearch();
 
 // GET LATITUDE AND LONGITUDE FOR ENTERED LOCATION
 
-let getLatLong = (location) => {
-	fetch(
-		'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-			encodeURIComponent(location) +
-			'.json?access_token=pk.eyJ1IjoicGV0bWFwNTUiLCJhIjoiY2s3NmZiYzl2MHFpZTNtbzNycHh0aDlnMCJ9.oERa5PVzkVL4oEZXMOWnxA&limit=1'
-	).then((response) => {
-		response.json().then((data) => {
-			let lat = data.features[0].bbox[1];
-			let lon = data.features[0].bbox[0];
-			fetchData(lat, lon);
-		});
+let getLatLong = async (location) => {
+	await axios.get(`/mapbox/${location}`).then((response) => {
+		const data = response.data;
+		let lat = data.features[0].bbox[1];
+		let lon = data.features[0].bbox[0];
+		fetchData(lat, lon);
 	});
 };
 
@@ -65,7 +53,7 @@ let lon = -121.943692018703;
 // GET FORECAST WITH THE LATITUDE AND LONGITUDE
 
 async function fetchData(lat, lon) {
-	const testingData = await axios
+	await axios
 		.get(`/weatherdark/${lat},${lon}`)
 		.then((response) => {
 			display.push({ 'Week Summary': response.data.daily.summary });
@@ -94,47 +82,36 @@ async function fetchData(lat, lon) {
 // GET ACCUWEATHER DATA FOR THE POLLEN LEVELS
 
 let getAccu = async (location) => {
-	await fetch(
-		'https://dataservice.accuweather.com/locations/v1/cities/search?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&q=' +
-			location +
-			'&details=true'
-	).then((response) => {
-		response.json().then((data) => {
-			let locationKey = data[0].Key;
-			pollenForecast(locationKey);
-		});
-	});
-	let pollenForecast = (locationKey) => {
-		fetch(
-			'https://dataservice.accuweather.com/forecasts/v1/daily/1day/' +
-				locationKey +
-				'?apikey=GJKGfMXiYFeHPUV3p3oHc28uvCAEvLTY&details=true'
-		).then((response) => {
-			response.json().then((data) => {
-				display.push({
-					'Air Quality': `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
-						.AirAndPollen[0].Category}`
-				});
-				display.push({
-					Grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
-						.Category}`
-				});
-				display.push({
-					Mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
-						.Category}`
-				});
-				display.push({
-					Ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
-						.Category}`
-				});
-				display.push({
-					Tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
-						.Category}`
-				});
-				// displayData();
+	const pollenForecast = async (locationKey) => {
+		await axios.get(`/accuweather/${locationKey}`).then((response) => {
+			const data = response.data;
+			display.push({
+				'Air Quality': `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0]
+					.AirAndPollen[0].Category}`
 			});
+			display.push({
+				Grass: `${data.DailyForecasts[0].AirAndPollen[1].Value} - ${data.DailyForecasts[0].AirAndPollen[1]
+					.Category}`
+			});
+			display.push({
+				Mold: `${data.DailyForecasts[0].AirAndPollen[2].Value} - ${data.DailyForecasts[0].AirAndPollen[2]
+					.Category}`
+			});
+			display.push({
+				Ragweed: `${data.DailyForecasts[0].AirAndPollen[3].Value} - ${data.DailyForecasts[0].AirAndPollen[3]
+					.Category}`
+			});
+			display.push({
+				Tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
+					.Category}`
+			});
+			// displayData();
 		});
 	};
+	await axios.get(`/accu/${location}`).then((response) => {
+		let locationKey = response.data[0].Key;
+		pollenForecast(locationKey);
+	});
 };
 
 // DISPLAY DATA ON THE PAGE
