089576acaa412b2872b64470408a15eb64d620c5
Tim Petrvalsky <tpet55@yahoo.com>
2021-03-29 11:30:09 -0700

    Fix display of all data

--- static/weather.js
+++ static/weather.js
@@ -10,8 +10,6 @@ const messageTwo = document.querySelector('#message-two');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
-let display = [];
-
 weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	display = [];
@@ -38,13 +36,11 @@ weatherForm.addEventListener('submit', (e) => {
 // GET LATITUDE AND LONGITUDE FOR ENTERED LOCATION
 
 let getLatLong = async (location) => {
-	console.log('location', location);
 	await axios.get(`/mapbox/${location}`).then((response) => {
 		const data = response.data;
-		console.log(data, 'data in mapbox route');
 		let lat = data.features[0].bbox[1];
 		let lon = data.features[0].bbox[0];
-		fetchData(lat, lon);
+		fetchData(lat, lon, location);
 	});
 };
 
@@ -52,9 +48,9 @@ let lat = 36.5964139832728;
 let lon = -121.943692018703;
 
 // GET FORECAST WITH THE LATITUDE AND LONGITUDE
-
-async function fetchData(lat, lon) {
-	const testingData = await axios
+let display = [];
+async function fetchData(lat, lon, location) {
+	await axios
 		.get(`/weatherdark/${lat},${lon}`)
 		.then((response) => {
 			display.push({ weekSummary: response.data.daily.summary });
@@ -73,10 +69,12 @@ async function fetchData(lat, lon) {
 			display.push({ visibility: response.data.currently.visibility });
 			display.push({ ozone: response.data.currently.ozone });
 			display.push({ sunrise: new Date(response.data.daily.data[0].sunriseTime * 1000).toLocaleString() });
-			// displayData(display);
 			display.push({ sunset: new Date(response.data.daily.data[0].sunsetTime * 1000).toLocaleString() });
-			// displayData(display);
-			temp(display);
+			// let waitDisplay = async () => {
+			// 	const delayDisplay = await display;
+			// 	console.log(delayDisplay, 'delayDisplay will this work?');
+			// };
+			// waitDisplay();
 		})
 		.catch((error) => {
 			console.log(error);
@@ -89,7 +87,6 @@ async function getAccu(location) {
 	const pollenForecast = async (locationKey) => {
 		await axios.get(`/accuweather/${locationKey}`).then((response) => {
 			const data = response.data;
-			console.log(data, 'pollen data');
 			display.push({
 				airQuality: `${data.DailyForecasts[0].AirAndPollen[0].Value} - ${data.DailyForecasts[0].AirAndPollen[0]
 					.Category}`
@@ -110,7 +107,7 @@ async function getAccu(location) {
 				tree: `${data.DailyForecasts[0].AirAndPollen[4].Value} - ${data.DailyForecasts[0].AirAndPollen[4]
 					.Category}`
 			});
-			// displayData();
+			temp(display);
 		});
 	};
 	await axios.get(`/accu/${location}`).then((response) => {
@@ -121,12 +118,12 @@ async function getAccu(location) {
 
 // DISPLAY DATA ON THE PAGE
 
-function temp(display) {
+async function temp(display) {
 	let toggle = document.querySelectorAll('.hidden');
 	toggle.forEach((item) => {
 		item.classList.toggle('hidden');
 	});
-	console.log(display, 'display');
+
 	let tempMin = document.getElementById('temp-min');
 	let tempMax = document.getElementById('temp-max');
 	let weekSummary = document.getElementById('week-summary');
@@ -147,20 +144,21 @@ function temp(display) {
 	let currentTemp = document.getElementById('current-temp');
 	let visibility = document.getElementById('visibility');
 	let ozone = document.getElementById('ozone');
-	let weatherObj = {};
 
+	let weatherObj = {};
+	// console.log(display, 'display at start of weatherObj creation');
 	for (let i = 0; i < display.length; i++) {
 		for (item in display[i]) {
 			weatherObj[item] = display[i][item];
 		}
 	}
-
+	// console.log(weatherObj, 'weatherObj before textContent section');
 	tempMin.textContent = `${~~weatherObj['minTemp']}°`;
 	tempMax.textContent = `${~~weatherObj['maxTemp']}°`;
 	weekSummary.textContent = weatherObj['weekSummary'];
 	airQuality.textContent = weatherObj['airQuality'];
 	grass.textContent = weatherObj['grass'];
-	console.log(grass.textContent, 'grass.textContent');
+	// console.log(grass.textContent, 'grass.textContent');
 	mold.textContent = weatherObj['mold'];
 	ragweed.textContent = weatherObj['ragweed'];
 	tree.textContent = weatherObj['tree'];
